229e875a39ec033b7827a3cb6904f33af27918db
Priyanka Ganesan <gpriyankapms@gmail.com>
2022-03-15 20:45:11 -0500

    Added home control

--- apps/pa-web/src/app/app.tsx
+++ apps/pa-web/src/app/app.tsx
@@ -1,11 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+import Home from './home/home';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="pa-web" />
+      <Home/>
       <div />
     </>
   );
---
+++ apps/pa-web/src/app/home/home.tsx
@@ -0,0 +1,22 @@
+
+import styles from './home.module.css';
+
+/* eslint-disable-next-line */
+export interface HomeProps {}
+
+export function Home(props: HomeProps) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Home!</h1>
+      Welocme
+      <ul>
+        <li>
+          
+        </li>
+      </ul>
+
+    </div>
+  );
+}
+
+export default Home;
